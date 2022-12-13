2ebd9f17ecee7d11db82497e08fead0f99f5adc8
kerry91 <kerry91warnes@gmail.com>
2022-12-13 13:13:53 +0000

    .

--- src/Main.js
+++ src/Main.js
@@ -1,27 +1,27 @@
-import React, {useState, useEffect} from "react";
-import { getPokemon, getAllPokemon } from './helpers/Pokemon';
+import React, { useState, useEffect } from "react";
+import { getPokemon, getAllPokemon } from "./helpers/Pokemon";
 
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 
 const Main = () => {
-  const [pokemonData, setPokemonData] = useState([])
-  const [nextUrl, setNextUrl] = useState('');
-  const [prevUrl, setPrevUrl] = useState('');
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
   // eslint-disable-next-line no-unused-vars
   const [loading, setLoading] = useState(true);
-  const initialURL = 'https://pokeapi.co/api/v2/pokemon'
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
 
   useEffect(() => {
     async function fetchData() {
-      let response = await getAllPokemon(initialURL)
+      let response = await getAllPokemon(initialURL);
       setNextUrl(response.next);
       setPrevUrl(response.previous);
       await loadPokemon(response.results);
       setLoading(false);
     }
     fetchData();
-  }, [])
+  }, []);
 
   const next = async () => {
     setLoading(true);
@@ -30,7 +30,7 @@ const Main = () => {
     setNextUrl(data.next);
     setPrevUrl(data.previous);
     setLoading(false);
-  }
+  };
 
   const prev = async () => {
     if (!prevUrl) return;
@@ -40,32 +40,38 @@ const Main = () => {
     setNextUrl(data.next);
     setPrevUrl(data.previous);
     setLoading(false);
-  }
+  };
 
   const loadPokemon = async (data) => {
-    let _pokemonData = await Promise.all(data.map(async pokemon => {
-      let pokemonRecord = await getPokemon(pokemon)
-      return pokemonRecord
-    }))
+    let _pokemonData = await Promise.all(
+      data.map(async (pokemon) => {
+        let pokemonRecord = await getPokemon(pokemon);
+        return pokemonRecord;
+      })
+    );
     setPokemonData(_pokemonData);
-  }
-
+  };
 
   return (
     <>
       <div className="Navbar">
         <Navbar />
       </div>
- 
+
       <div className="card container">
-      {pokemonData.map((pokemon, i) => {
-                return <Card key={i} pokemon={pokemon} />
-              })}
+        {pokemonData.map((pokemon, i) => {
+          return <Card key={i} pokemon={pokemon} />;
+        })}
+        
       </div>
       <div className="pagination">
-              <button class="pbutton" onClick={prev}>Prev</button>
-              <button class="pbutton" onClick={next}>Next</button>
-      </div>     
+        <button class="pbutton" onClick={prev}>
+          Prev
+        </button>
+        <button class="pbutton" onClick={next}>
+          Next
+        </button>
+      </div>
     </>
   );
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React from "react";
 
-
-function Card({ pokemon}) {
-  const style = `poke-card ${pokemon.type}`;
-    return (
+function Card({ pokemon }) {
+  
+  const style = `poke-card ${pokemon.types[0].type.name}`;
+  return (
+    <div className="overlay-wrapper">
       <div className={style}>
-            <div className="poster">
-                <img class="poster" src={pokemon.sprites.other.dream_world.front_default} alt="" />
+        <div className="poster">
+          <img
+            class="poster"
+            src={pokemon.sprites.other.dream_world.front_default}
+            alt=""
+          />
+          <div class="overlay">
+            <div class="text">
+              Type: {pokemon.types[0].type.name}<br/>
+              Ability: {pokemon.abilities[0].ability.name}<br/>
+              Height: {pokemon.height}<br/>
+              Weight: {pokemon.weight}<br/>
             </div>
-            <div className="poke-details">
-            <div className="box">
-               #{pokemon.id}
-               <br/>
-                {pokemon.name}
-            </div>
-            </div>
-            </div>
-
-    );
+          </div>
+        </div>
+        <div className="poke-details">
+          <div className="box">
+            No.{pokemon.id}
+            <br />
+            {pokemon.name}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
